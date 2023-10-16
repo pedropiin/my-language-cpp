5b977030bf3b4b604ad9c3a20db80446a9c089af
pedropiin <pedarosa04@gmail.com>
2023-10-16 20:07:34 -0300

    testando gitignore

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Users/User/Desktop/all/dev/projects/asm-compiler/src
+# Install script for directory: /mnt/c/Users/Pedro/desktop/all/dev/projects/asm-compiler/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Users/User/Desktop/all/dev/projects/asm-compiler/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/Pedro/desktop/all/dev/projects/asm-compiler/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
